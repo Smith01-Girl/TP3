9c93c013e04348fe223dc2a92a4b5fbb9f630891
la paix <2237789@cegeplimoilou.ca>
2025-12-13 21:50:16 -0500

    Créer un microservice Critères #3

--- gateway/src/main/java/web2/tp3/web2/GatewayApplication.java
+++ gateway/src/main/java/web2/tp3/web2/GatewayApplication.java
@@ -1,4 +1,4 @@
-package web2.tp3.web2.tp3;
+package web2.tp3.web2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +9,24 @@ import org.springframework.web.servlet.function.ServerResponse;
 import static org.springframework.web.servlet.function.RequestPredicates.path;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
+
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+  /*  @Bean
+    public RouterFunction<ServerResponse> getRoutes() {
+        return route()
+                // Route 1 : Vers Service Critères
+                .route(path("/criteres/**"), http("http://service-criteres:8083"))
+
+                // Route 2 : Vers Service Statistiques (AJOUTÉE)
+                .route(path("/statistiques/**"), http("http://service-statistiques:8081"))
+
+                .build();
+    }*/
 
 
 }
---
+++ service-statistiques/src/main/java/web2/tp3/ServiceStatistiquesApplication.java
@@ -0,0 +1,4 @@
+package web2.tp3;
+
+public class ServiceStatistiquesApplication {
+}
