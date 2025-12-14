2675672dde8e41a1de04e1937db86997306b8bf4
2143391 <2143391@cegeplimoilou.ca>
2025-12-14 11:47:51 -0500

    Ajout des files sur Nouvelles du tp2  #2

--- service-nouvelles/src/main/java/web2/tp3/ServiceNouvellesApplication.java
+++ service-nouvelles/src/main/java/web2/tp3/ServiceNouvellesApplication.java
@@ -1,17 +1,13 @@
 package web2.tp3;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
-@AllArgsConstructor
 @SpringBootApplication
-public class Tp2webApplication{
+public class ServiceNouvellesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tp2webApplication.class, args);
+		SpringApplication.run(ServiceNouvellesApplication.class, args);
 
 	}
 }
---
+++ service-nouvelles/src/main/java/web2/tp3/model/Nouvelle.java
@@ -0,0 +1,53 @@
+package web2.tp3.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Nouvelle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Un titre est requis")
+    @Size(min = 4, max = 75, message = "Le titre doit être entre 4 et 75 caractères")
+    @Column(unique = true, nullable = false)
+    private String titre;
+
+    @Column(nullable = false)
+    private String src;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Size(max = 300, message = "Le résumé doit être moins de 300 caractères")
+    @Column(nullable = false)
+    private String resume;
+
+    @Size(max = 250, message = "Le texte doit être moins de 150 caractères")
+    @Column(nullable = false)
+    private String texte;
+
+    @Column(nullable = false)
+    private Integer popularite = 0;
+
+    public Nouvelle(String titre, String src, LocalDate date, String resume, String texte) {
+        this.titre = titre;
+        this.src = src;
+        this.date = date;
+        this.resume = resume;
+        this.texte = texte;
+    }
+}
